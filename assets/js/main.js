a4b4d1e751f29e0755dbe2dda3af7028c3e85b15
kksfia <89885039+kksfia@users.noreply.github.com>
2021-09-13 11:00:19 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -99,8 +99,8 @@ app.component('answer-input', {
   data: function () {
     return {
       /* 送信ボタン上下に表示されるメッセージ */
-      okMessage: '正解！',
-      ngMessage: 'そのキーワードは違うようだぞ！？',
+      okMessage: '認証に成功しました！',
+      ngMessage: 'パスワードが違います。',
       message: '',
       inputAnswer: '',
     }
